a62db11a1ec09901e97526800b582b2d3b76b061
Liridona Sejdiu <liridoona.a@hotmail.com>
2022-11-22 15:20:41 -0600

    added a few tests for some contact methods based on previous discussions

---
+++ lahuta/tests/test_contacts.py
@@ -0,0 +1,74 @@
+import warnings
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+import lahuta.contacts.contacts as contacts
+from lahuta.core.neighbors import NeighborPairs
+from lahuta.core.universe import Universe
+
+
+class ExpectedResults:
+    COVALENT = np.array(
+        [[805, 1174], [1204, 238], [916, 880], [1196, 198], [1174, 252], [43, 26]]
+    )
+    METALIC = np.array([[805, 1174], [1174, 252]])
+    CARBONYL = np.array(
+        [[802, 751], [759, 801], [236, 290], [998, 1039], [129, 195], [666, 632]]
+    )
+    HBOND = np.array(
+        [[749, 700], [749, 689], [844, 716], [844, 700], [851, 819], [697, 632]]
+    )
+    WEAK_HBOND = np.array(
+        [[651, 982], [651, 701], [916, 30], [916, 114], [583, 530], [985, 702]]
+    )
+    IONIC = np.array(
+        [[721, 942], [1159, 447], [1189, 515], [1189, 514], [1189, 513], [1189, 512]]
+    )
+
+
+class DataLoader:
+    def __init__(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.u = Universe(Path(__file__).parent / "data" / "1KX2.pdb")
+        self.n = self.u.compute_neighbors()
+
+    def __call__(self):
+        return self.u, self.n
+
+
+@pytest.fixture
+def neighbors():
+    return DataLoader().n
+
+
+def test_covalent_neighbors(neighbors):
+    cov = contacts.covalent_neighbors(neighbors)
+    assert np.all(cov.pairs == ExpectedResults.COVALENT)
+
+
+def test_metalic_neighbors(neighbors):
+    met = contacts.metalic_neighbors(neighbors)
+    assert np.all(met.pairs == ExpectedResults.METALIC)
+
+
+def test_carbonyl_neighbors(neighbors):
+    carb = contacts.carbonyl_neighbors(neighbors)
+    assert np.all(carb.pairs[:6] == ExpectedResults.CARBONYL)
+
+
+def test_hbond_neighbors(neighbors):
+    hb = contacts.hbond_neighbors(neighbors)
+    assert np.all(hb.pairs[:6] == ExpectedResults.HBOND)
+
+
+def test_weak_hbond_neighbors(neighbors):
+    whb = contacts.weak_hbond_neighbors(neighbors)
+    assert np.all(whb.pairs[:6] == ExpectedResults.WEAK_HBOND)
+
+
+def test_ionic_neighbors(neighbors):
+    ionic = contacts.ionic_neighbors(neighbors)
+    assert np.all(ionic.pairs[:6] == ExpectedResults.IONIC)
