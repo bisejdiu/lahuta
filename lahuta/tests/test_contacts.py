93de3542785d021f4f8d0aa55325796c5423e20d
Liridona Sejdiu <liridoona.a@hotmail.com>
2022-11-22 16:33:42 -0600

    documented the test functions

--- lahuta/tests/test_contacts.py
+++ lahuta/tests/test_contacts.py
@@ -10,7 +10,6 @@ from lahuta.core.neighbors import NeighborPairs
 from lahuta.core.universe import Universe
 
 
-# we will load the data from the json file
 class ExpectedResults:
     with open(Path(__file__).parent / "data" / "1KX2.json") as f:
         data = json.load(f)
@@ -28,22 +27,30 @@ class ExpectedResults:
 
 
 class DataLoader:
+    """
+    Class to load the data for the tests.
+    """
+
     def __init__(self):
+        """Load the data for the tests."""
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             self.u = Universe(Path(__file__).parent / "data" / "1KX2.pdb")
         self.n = self.u.compute_neighbors()
 
     def __call__(self):
+        """Return the data."""
         return self.u, self.n
 
 
 @pytest.fixture
 def neighbors():
+    """Helper fixture to get neighbor pairs."""
     return DataLoader().n
 
 
 def test_covalent_neighbors(neighbors):
+    """Test the covalent neighbors."""
     cov = contacts.covalent_neighbors(neighbors)
     pairs, distances = np.array(cov.pairs[:6]), np.array(cov.distances[:6])
     assert cov.pairs.shape[0] == ExpectedResults.COVALENT["shapex"]
@@ -52,6 +59,7 @@ def test_covalent_neighbors(neighbors):
 
 
 def test_metalic_neighbors(neighbors):
+    """Test the metalic neighbors."""
     met = contacts.metalic_neighbors(neighbors)
     pairs, distances = np.array(met.pairs), np.array(met.distances)
     assert met.pairs.shape[0] == ExpectedResults.METALIC["shapex"]
@@ -60,30 +68,16 @@ def test_metalic_neighbors(neighbors):
 
 
 def test_carbonyl_neighbors(neighbors):
-    carb = contacts.carbonyl_neighbors(neighbors)
-    pairs, distances = np.array(carb.pairs[:6]), np.array(carb.distances[:6])
-    assert carb.pairs.shape[0] == ExpectedResults.CARBONYL["shapex"]
+    """Test the carbonyl neighbors."""
+    cbnyl = contacts.carbonyl_neighbors(neighbors)
+    pairs, distances = np.array(cbnyl.pairs[:6]), np.array(cbnyl.distances[:6])
+    assert cbnyl.pairs.shape[0] == ExpectedResults.CARBONYL["shapex"]
     assert np.all(pairs == ExpectedResults.CARBONYL["pairs"])
     assert np.allclose(distances, ExpectedResults.CARBONYL["distances"], atol=1e-3)
 
 
-def test_hbond_neighbors(neighbors):
-    hb = contacts.hbond_neighbors(neighbors)
-    pairs, distances = np.array(hb.pairs[:6]), np.array(hb.distances[:6])
-    assert hb.pairs.shape[0] == ExpectedResults.HBOND["shapex"]
-    assert np.all(pairs == ExpectedResults.HBOND["pairs"])
-    assert np.allclose(distances, ExpectedResults.HBOND["distances"], atol=1e-3)
-
-
-def test_weak_hbond_neighbors(neighbors):
-    whb = contacts.weak_hbond_neighbors(neighbors)
-    pairs, distances = np.array(whb.pairs[:6]), np.array(whb.distances[:6])
-    assert whb.pairs.shape[0] == ExpectedResults.WEAK_HBOND["shapex"]
-    assert np.all(pairs == ExpectedResults.WEAK_HBOND["pairs"])
-    assert np.allclose(distances, ExpectedResults.WEAK_HBOND["distances"], atol=1e-3)
-
-
 def test_ionic_neighbors(neighbors):
+    """Test the ionic neighbors."""
     ionic = contacts.ionic_neighbors(neighbors)
     pairs, distances = np.array(ionic.pairs[:6]), np.array(ionic.distances[:6])
     assert ionic.pairs.shape[0] == ExpectedResults.IONIC["shapex"]
@@ -92,6 +86,7 @@ def test_ionic_neighbors(neighbors):
 
 
 def test_aromatic_neighbors(neighbors):
+    """Test the aromatic neighbors."""
     aromatic = contacts.aromatic_neighbors(neighbors)
     pairs, distances = np.array(aromatic.pairs[:6]), np.array(aromatic.distances[:6])
     assert aromatic.pairs.shape[0] == ExpectedResults.AROMATIC["shapex"]
@@ -100,6 +95,7 @@ def test_aromatic_neighbors(neighbors):
 
 
 def test_hydrophobic_neighbors(neighbors):
+    """Test the hydrophobic neighbors."""
     hydrophobic = contacts.hydrophobic_neighbors(neighbors)
     pairs, distances = np.array(hydrophobic.pairs[:6]), np.array(
         hydrophobic.distances[:6]
@@ -109,7 +105,35 @@ def test_hydrophobic_neighbors(neighbors):
     assert np.allclose(distances, ExpectedResults.HYDROPHOBIC["distances"])
 
 
+def test_vdw_neighbors(neighbors):
+    """Test the vdw neighbors."""
+    vdw = contacts.vdw_neighbors(neighbors)
+    pairs, distances = np.array(vdw.pairs[:6]), np.array(vdw.distances[:6])
+    assert vdw.pairs.shape[0] == ExpectedResults.VDW["shapex"]
+    assert np.all(pairs == ExpectedResults.VDW["pairs"])
+    assert np.allclose(distances, ExpectedResults.VDW["distances"])
+
+
+def test_hbond_neighbors(neighbors):
+    """Test the hbond neighbors."""
+    hb = contacts.hbond_neighbors(neighbors)
+    pairs, distances = np.array(hb.pairs[:6]), np.array(hb.distances[:6])
+    assert hb.pairs.shape[0] == ExpectedResults.HBOND["shapex"]
+    assert np.all(pairs == ExpectedResults.HBOND["pairs"])
+    assert np.allclose(distances, ExpectedResults.HBOND["distances"], atol=1e-3)
+
+
+def test_weak_hbond_neighbors(neighbors):
+    """Test the weak hbond neighbors."""
+    whb = contacts.weak_hbond_neighbors(neighbors)
+    pairs, distances = np.array(whb.pairs[:6]), np.array(whb.distances[:6])
+    assert whb.pairs.shape[0] == ExpectedResults.WEAK_HBOND["shapex"]
+    assert np.all(pairs == ExpectedResults.WEAK_HBOND["pairs"])
+    assert np.allclose(distances, ExpectedResults.WEAK_HBOND["distances"], atol=1e-3)
+
+
 def test_polar_hbond_neighbors(neighbors):
+    """Test the polar hbond neighbors."""
     phb = contacts.polar_hbond_neighbors(neighbors)
     pairs, distances = np.array(phb.pairs[:6]), np.array(phb.distances[:6])
     assert phb.pairs.shape[0] == ExpectedResults.POLAR_HBOND["shapex"]
@@ -118,16 +142,9 @@ def test_polar_hbond_neighbors(neighbors):
 
 
 def test_weak_polar_hbond_neighbors(neighbors):
+    """Test the weak polar hbond neighbors."""
     pwhb = contacts.weak_polar_hbond_neighbors(neighbors)
     pairs, distances = np.array(pwhb.pairs[:6]), np.array(pwhb.distances[:6])
     assert pwhb.pairs.shape[0] == ExpectedResults.WEAK_POLAR_HBOND["shapex"]
-    assert np.all(np.array(pwhb.pairs[:6]) == ExpectedResults.WEAK_POLAR_HBOND["pairs"])
+    assert np.all(pairs == ExpectedResults.WEAK_POLAR_HBOND["pairs"])
     assert np.allclose(distances, ExpectedResults.WEAK_POLAR_HBOND["distances"])
-
-
-def test_vdw_neighbors(neighbors):
-    vdw = contacts.vdw_neighbors(neighbors)
-    pairs, distances = np.array(vdw.pairs[:6]), np.array(vdw.distances[:6])
-    assert vdw.pairs.shape[0] == ExpectedResults.VDW["shapex"]
-    assert np.all(pairs == ExpectedResults.VDW["pairs"])
-    assert np.allclose(distances, ExpectedResults.VDW["distances"])
