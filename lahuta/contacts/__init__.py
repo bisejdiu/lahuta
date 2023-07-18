f556871661d9df2f746eb20b47784cb651d5874f
Besian Sejdiu <besiansejdiu@gmail.com>
2023-07-18 00:50:25 -0500

    added all imports and defined F to contain functionals

--- lahuta/contacts/__init__.py
+++ lahuta/contacts/__init__.py
@@ -1,17 +1,5 @@
-# from .aromatic import AromaticContacts
-# from .carbonyl import CarbonylContacts
-# from .covalent import CovalentContacts
-# from .hbonds import (
-#     HBondContacts,
-#     WeakHBondContacts,
-#     PolarHBondContacts,
-#     WeakPolarHBondContacts,
-# )
-# from .hydrophobic import HydrophobicContacts
-# from .ionic import IonicContacts
-# from .metal import MetalContacts
-# from .vdw import VanDerWaalsContacts
-
+from .aromatic import AromaticContacts
+from .carbonyl import CarbonylContacts
 from .contacts import (
     aromatic_neighbors,
     carbonyl_neighbors,
@@ -25,6 +13,36 @@ from .contacts import (
     weak_hbond_neighbors,
     weak_polar_hbond_neighbors,
 )
+from .covalent import CovalentContacts
+from .hbonds import (
+    HBondContacts,
+    PolarHBondContacts,
+    WeakHBondContacts,
+    WeakPolarHBondContacts,
+)
+from .hydrophobic import HydrophobicContacts
+from .ionic import IonicContacts
+from .metal import MetalicContacts
+from .vdw import VanDerWaalsContacts
+
+funcs = [
+    aromatic_neighbors,
+    carbonyl_neighbors,
+    covalent_neighbors,
+    hbond_neighbors,
+    hydrophobic_neighbors,
+    ionic_neighbors,
+    metalic_neighbors,
+    polar_hbond_neighbors,
+    vdw_neighbors,
+    weak_hbond_neighbors,
+    weak_polar_hbond_neighbors,
+]
+
+
+F = type("F", (), {})
+for func in funcs:
+    setattr(F, func.__name__, func)
 
 __all__ = [
     "aromatic_neighbors",
@@ -38,4 +56,15 @@ __all__ = [
     "vdw_neighbors",
     "weak_hbond_neighbors",
     "weak_polar_hbond_neighbors",
+    "AromaticContacts",
+    "CarbonylContacts",
+    "CovalentContacts",
+    "HBondContacts",
+    "HydrophobicContacts",
+    "IonicContacts",
+    "MetalContacts",
+    "PolarHBondContacts",
+    "VanDerWaalsContacts",
+    "WeakHBondContacts",
+    "WeakPolarHBondContacts",
 ]
