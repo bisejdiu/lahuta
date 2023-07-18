15eebc36556cfbde530fed8face89f549192fa37
Besian Sejdiu <besiansejdiu@gmail.com>
2023-07-18 00:12:08 -0500

    added a Base class definition for the updated class-based contact definition

--- lahuta/contacts/base.py
+++ lahuta/contacts/base.py
@@ -1,18 +1,36 @@
-from abc import ABC, abstractmethod
-
-from lahuta.core.neighbors import NeighborPairs
+from typing import Any, Protocol, runtime_checkable
 
 
-class ContactAnalysis(NeighborPairs):
+@runtime_checkable
+class ComputeProtocol(Protocol):
+    def compute(self) -> Any:
+        ...
+
+
+@runtime_checkable
+class ComputeElementwiseProtocol(Protocol):
+    def compute_elementwise(self, atoms, pair, distance) -> Any:
+        ...
+
+
+class ContactAnalysis:
     def __init__(self, ns):
         self.ns = ns
-        super().__init__(ns.luni, ns.pairs, ns.distances)
+        self.partner1_atoms = ns.atoms
+        self.results: Any = None
 
-        # self._results = self._compute(self.ns.luni.to("mda").atoms, self.pairs, self.distances)
-        self._results = self._compute(
-            self.ns.partner1, self.ns.partner2, self.distances
-        )
+    def run(self):
+        self.run_methods()
 
-    @abstractmethod
-    def _compute(self, atoms, pairs, distances):
-        ...
+    def run_methods(self):
+        if isinstance(self, ComputeProtocol):
+            self.results = self.compute()
+        elif isinstance(self, ComputeElementwiseProtocol):
+            self.results = []
+            p1_atoms, p2_atoms = self.ns.partner1, self.ns.partner2
+            for atom1, atom2, distance in zip(p1_atoms, p2_atoms, self.ns.distances):
+                self.results.append(self.compute_elementwise(atom1, atom2, distance))
+        else:
+            raise NotImplementedError(
+                "Object must implement either compute or compute_elementwise methods."
+            )
