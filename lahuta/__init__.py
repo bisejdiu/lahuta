3d28ca8218b04abc09e378cad5eb3b959f035ad7
Besian Sejdiu <besiansejdiu@gmail.com>
2023-08-07 10:44:34 -0500

    improvements to API/core TOC and formatting changes

--- lahuta/__init__.py
+++ lahuta/__init__.py
@@ -1,8 +1,8 @@
-from lahuta.core import NeighborPairs, Universe
+from lahuta.core import ARC, Atom, Atoms, Chains, NeighborPairs, Residues, Universe
 
 from ._version import VERSION
 
 __version__ = VERSION
 
-__all__ = ['Universe', 'NeighborPairs']
+__all__ = ['Universe', 'NeighborPairs', 'Atom', 'Atoms', 'Residues', 'Chains', 'ARC']
 __all__ += ['VERSION', '__version__']
--- lahuta/core/__init__.py
+++ lahuta/core/__init__.py
@@ -3,8 +3,9 @@ The `core` module contains the core functionality of Lahuta.
 
 """
 
+from lahuta.core.arc import ARC, Atom, Atoms, Chains, Residues
 from lahuta.core.neighbor_finder import NeighborSearch
 from lahuta.core.neighbors import NeighborPairs
 from lahuta.core.universe import LuniInputType, Universe
 
-__all__ = ['LuniInputType', 'Universe', 'NeighborPairs', 'NeighborSearch']
+__all__ = ['LuniInputType', 'Universe', 'NeighborPairs', 'NeighborSearch', 'ARC', 'Atoms', 'Residues', 'Chains', 'Atom']
