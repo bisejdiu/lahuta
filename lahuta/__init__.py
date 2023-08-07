08ca712c9dbc00f36865e32e584e53512bf61498
Besian Sejdiu <besiansejdiu@gmail.com>
2023-08-07 09:58:58 -0500

    simplifies imports and corrects docs for autogen

--- lahuta/__init__.py
+++ lahuta/__init__.py
@@ -1,5 +1,8 @@
+from lahuta.core import NeighborPairs, Universe
+
 from ._version import VERSION
 
 __version__ = VERSION
 
-__all__ = ['VERSION', '__version__']
+__all__ = ['Universe', 'NeighborPairs']
+__all__ += ['VERSION', '__version__']
--- lahuta/core/__init__.py
+++ lahuta/core/__init__.py
@@ -2,3 +2,9 @@
 The `core` module contains the core functionality of Lahuta.
 
 """
+
+from lahuta.core.neighbor_finder import NeighborSearch
+from lahuta.core.neighbors import NeighborPairs
+from lahuta.core.universe import LuniInputType, Universe
+
+__all__ = ['LuniInputType', 'Universe', 'NeighborPairs', 'NeighborSearch']
