f2496e98e22b60ce53c4d2459adafa4bc706deb0
Besian Sejdiu <besiansejdiu@gmail.com>
2023-08-30 02:49:45 -0500

    simplify imports to the api module

--- lahuta/api/__init__.py
+++ lahuta/api/__init__.py
@@ -5,6 +5,16 @@ They should make it easy to use the package in a variety of contexts
 (e.g. processing files, computing neighbor pairs, etc.)
 """
 
+from lahuta.api.processors import CachedFileProcessor, FileProcessor
 from lahuta.api.utils import download_structures
+from lahuta.api.workers import EmptyWorker, NeighborPairsComputer, SequenceComputer, WorkerStrategy
 
-__all__ = ["download_structures"]
+__all__ = [
+    "download_structures",
+    "CachedFileProcessor",
+    "FileProcessor",
+    "WorkerStrategy",
+    "EmptyWorker",
+    "NeighborPairsComputer",
+    "SequenceComputer",
+]
