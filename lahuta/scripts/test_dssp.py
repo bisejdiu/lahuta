ea16596db4b6b68e03259a6f7acd3c5bad55566c
Besian Sejdiu <besiansejdiu@gmail.com>
2023-11-07 23:50:05 -0600

    refactored dssp code: we only need to know about one class now and it handles dssp calculation & parsing

--- lahuta/scripts/test_dssp.py
+++ lahuta/scripts/test_dssp.py
@@ -1,6 +1,8 @@
 import io
+import shutil
 import subprocess
-from typing import Any
+from typing import Any, Optional
+from pathlib import Path
 
 import numpy as np
 from numpy.typing import NDArray
@@ -8,8 +10,20 @@ from Bio.PDB.DSSP import _make_dssp_dict
 
 
 class DSSP:
-    def __init__(self, dssp_dict: dict[tuple[str, tuple[str, int, str]], tuple[Any, ...]]) -> None:
-        self.dssp_dict = dssp_dict
+    def __init__(
+        self,
+        input_file: Optional[str | Path] = None,
+        dssp_dict: Optional[dict[tuple[str, tuple[str, int, str]], tuple[Any, ...]]] = None,
+    ) -> None:
+        match (input_file, dssp_dict):
+            case (None, None):
+                raise ValueError('Either input_file or dssp_dict must be provided.')
+            case (None, dssp_dict):
+                self.dssp_dict = dssp_dict or {}
+            case (input_file, None):
+                handler = DSSPCLIHandler(str(input_file))
+                handler.parse_or_calculate_dssp()
+                self.dssp_dict = handler.dssp_dict
 
     @property
     def secondary_structure(self) -> NDArray[np.str_]:
@@ -21,42 +35,47 @@ class DSSP:
         # Solvent accessible surface areas
         return np.array([data[2] for data in self.dssp_dict.values()])
 
+
 class DSSPCLIHandler:
-    def __init__(self, input_file):
+    def __init__(self, input_file: Path | str, executable_name: Optional[str] = None) -> None:
         self.input_file = input_file
+        self.executable_name = executable_name
+        self.dssp_dict: dict[tuple[str, tuple[str, int, str]], tuple[Any, ...]] = {}
         self._command = self._build_command()
 
-    def get_dssp_dict(self):
-        cli_output = self.run()
-        file_like_object = io.StringIO(cli_output)
+    def parse_or_calculate_dssp(self) -> None:
+        if not self.dssp_dict:
+            cli_output = self.run()
+            file_like_object = io.StringIO(cli_output)
 
-        dssp_dict, _ = _make_dssp_dict(file_like_object)
-        return dssp_dict
+            dssp_dict, _ = _make_dssp_dict(file_like_object)
+            self.dssp_dict = dssp_dict
 
-    def _build_command(self):
-        return ['mkdssp', '-i', self.input_file]
-        
-    def run(self):
+    def _build_command(self) -> list[str]:
+        executable_name = self.executable_name if self.executable_name else 'dssp'
+        return [executable_name, '-i', str(self.input_file)]
+
+    def run(self) -> str:
+        if shutil.which(self.command[0]) is None:
+            raise RuntimeError(f"Could not find the executable for {self.command[0]}")
         result = subprocess.run(self.command, capture_output=True, text=True, check=True)
         return result.stdout
 
     @property
-    def command(self):
+    def command(self) -> list[str]:
         return self._command
 
 
 if __name__ == '__main__':
-    # Test the DSSP class
-    # dssp_data = DSSP('/home/bisejdiu/2023/lahuta/1gzm.dssp')
-    # secondary_structure = dssp_data.secondary_structure
-    # print('secondary_structure', secondary_structure)
-    # solvent_accessible_area = dssp_data.solvent_accessible_area
-    # print('solvent_accessible_area', solvent_accessible_area)
+    file_loc = Path(__file__).parent.parent.parent / '1gzm.cif'
 
-    handler = DSSPCLIHandler('1gzm.cif')
-    dssp_dict = handler.get_dssp_dict()
-    dssp_data = DSSP(dssp_dict)
-    secondary_structure = dssp_data.secondary_structure
+    # handler = DSSPCLIHandler(str(file_loc))
+    # handler.parse_or_calculate_dssp()
+    # dssp = DSSP(dssp_dict=handler.dssp_dict)
+
+    dssp = DSSP(input_file=str(file_loc))
+
+    secondary_structure = dssp.secondary_structure
     print('secondary_structure', secondary_structure)
-    solvent_accessible_area = dssp_data.solvent_accessible_area
-    print('solvent_accessible_area', solvent_accessible_area)
+    # solvent_accessible_area = dssp.solvent_accessible_area
+    # print('solvent_accessible_area', solvent_accessible_area)
