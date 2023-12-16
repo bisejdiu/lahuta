3fb72cd91f7f2272f70d31c6218a153681d73c7c
Besian I. Sejdiu <besiansejdiu@gmail.com>
2023-12-16 16:58:22 -0600

    refactor(api): moved the Worker class to the processors file.

--- lahuta/api/processors.py
+++ lahuta/api/processors.py
@@ -3,14 +3,45 @@ import functools
 import logging
 import os
 from concurrent.futures import ProcessPoolExecutor
-from typing import Callable, Iterable, Optional, TypeVar
+from typing import Callable, Generic, Iterable, Optional, TypeVar
 
 from lahuta import Luni, NeighborPairs
-from lahuta.api.workers import Worker
 
 __all__ = ["CachedFileProcessor", "FileProcessor"]
 
 T = TypeVar("T")
+
+
+class Worker(Generic[T]):
+    """Worker strategy for file processing.
+
+    It takes a function that processes a file and returns a dictionary of file names and results.
+    It is used by the file processor classes to process files in parallel.
+
+    Attributes:
+        func (Callable[[str], T]): A function that processes a file and returns a dictionary of file names and results.
+
+    Methods:
+        execute: Execute the worker strategy.
+    """
+
+    def __init__(self, func: Callable[..., T]) -> None:
+        self.func = func
+
+    def execute(self, file_paths: list[str]) -> dict[str, T]:
+        """Execute the worker strategy.
+
+        Args:
+            file_paths (list[str]): list of file paths.
+
+        Returns:
+            dict[str, str]: Dictionary of sequences.
+        """
+        sequence_dict = {}
+        for file_path in file_paths:
+            file_name = os.path.basename(file_path)
+            sequence_dict[file_name] = self.func(file_path)
+        return sequence_dict
 
 
 class CachedFileProcessor:
@@ -23,8 +54,8 @@ class CachedFileProcessor:
         n_jobs (int): Number of workers.
 
     Methods:
-        walk: Walk through the directory and process the files.
-        results: Get the cached results.
+        process: Walk through the directory and process the files.
+
     """
 
     def __init__(
@@ -69,7 +100,7 @@ class CachedFileProcessor:
                 executor.submit(worker.execute, all_files[i : i + chunk_size]) for i in range(0, num_files, chunk_size)
             ]
 
-        result: dict[str, T] = {}  # = cast(dict[str, T], {})
+        result: dict[str, T] = {}
         for future in futures:
             result.update(future.result())
 
