f51fa8910f8ac7270e999193bd235c9452f78237
Besian I. Sejdiu <besiansejdiu@gmail.com>
2024-08-05 01:46:10 -0500

    removed old code for bond perception

--- src/bonds.cpp
+++ src/bonds.cpp
@@ -1,154 +1,27 @@
-#include <limits>
-
-#include "GraphMol/MonomerInfo.h"
+#include <GraphMol/PeriodicTable.h>
 #include "bonds.hpp"
 #include "convert.hpp"
-#include "nsgrid.hpp"
-
-#include "external/ob/elements.h"
-
 #include "bonds/bonds.hpp"
 #include "bonds/table.hpp"
 
-using namespace gemmi;
-
-bool connectVdW(Atom p, Atom q, double dist_sq, double covFactor) {
-  double rcov1 = covFactor * RDKit::PeriodicTable::getTable()->getRcovalent(
-                                 p.element.atomic_number());
-  double rcov2 = covFactor * RDKit::PeriodicTable::getTable()->getRcovalent(
-                                 q.element.atomic_number());
-  if (dist_sq <= (rcov1 + rcov2) * (rcov1 + rcov2)) {
-    return true;
-  }
-  return false;
-}
-
-static bool validAdditionalBond(RDKit::Atom *a, RDKit::Atom *b) {
-  if (a->getExplicitValence() == 5 && b->getAtomicNum() == 15) {
-
-    // only allow ochedral bonding for F and Cl
-    if (a->getAtomicNum() == 9 || a->getAtomicNum() == 17) {
-      return true;
-    } else {
-      return false;
-    }
-    // other things to check?
-    return true;
-  }
-
-  if (a->getAtomicNum() == 1 && b->getAtomicNum() == 1) {
-    return false;
-  }
-  if (a->getAtomicNum() == 1 && b->getAtomicNum() == 6) {
-    return false;
-  }
-  if (a->getAtomicNum() == 6 && b->getAtomicNum() == 1) {
-    return false;
-  }
-  return true;
-}
-
-bool connectVdW(RDKit::Atom p, RDKit::Atom q, double dist_sq,
-                double covFactor) {
-  if (dist_sq < 0.16) {
-    return false;
-  }
-  double _rcov1 = covFactor * RDKit::PeriodicTable::getTable()->getRcovalent(
-                                  p.getAtomicNum());
-  double _rcov2 = covFactor * RDKit::PeriodicTable::getTable()->getRcovalent(
-                                  q.getAtomicNum());
-  double rcov1 = covFactor * OBElements::GetCovalentRad(p.getAtomicNum());
-  double rcov2 = covFactor * OBElements::GetCovalentRad(q.getAtomicNum());
-
-  if (dist_sq <= (rcov1 + rcov2) * (rcov1 + rcov2)) {
-    return true;
-  }
-  return false;
-}
+auto PeriodicTable = RDKit::PeriodicTable::getTable();
 
 bool connectOBMol(RDKit::Atom *p, RDKit::Atom *q, double dist_sq,
                   double tolerance) {
   if (dist_sq < 0.16) {
     return false;
   }
-  // double _rcov1 = tolerance * RDKit::PeriodicTable::getTable()->getRcovalent(
-  //                                p.getAtomicNum());
-  // double _rcov2 = tolerance * RDKit::PeriodicTable::getTable()->getRcovalent(
-  //                                q.getAtomicNum());
-  double rcov1 = OBElements::GetCovalentRad(p->getAtomicNum());
-  double rcov2 = OBElements::GetCovalentRad(q->getAtomicNum());
+  double rcov1 = PeriodicTable->getRcovalent(p->getAtomicNum());
+  double rcov2 = PeriodicTable->getRcovalent(q->getAtomicNum());
+
   if (dist_sq <= (rcov1 + rcov2 + tolerance) * (rcov1 + rcov2 + tolerance)) {
     return true;
   }
   return false;
 }
-bool shouldSkip(RDKit::Atom &atom) {
-  auto explicitValence = atom.getExplicitValence();
-  if (atom.getExplicitValence() >=
-      OBElements::GetMaxBonds(atom.getAtomicNum())) {
-    return true;
-  }
-  if (atom.getAtomicNum() == 7 && atom.getFormalCharge() == 0 &&
-      atom.getExplicitValence() >= 3) {
-    return true;
-  }
-  return false;
-}
 
-void perceiveBonds(RDKit::RWMol &mol, RDKit::RWMol &newMol,
-                   std::vector<int> atomIndices, const NSResults &results,
-                   const float covFactor) {
-
-  for (auto i = 0; i < results.getNeighbors().size(); i++) {
-    auto res = results.getNeighbors()[i];
-    auto dist_sq = results.distances[i];
-
-    auto *a = mol.getAtomWithIdx(res.first);
-    auto *b = mol.getAtomWithIdx(res.second);
-
-    // NOTE: first let's handle only within non-protein atoms
-    auto aIt = std::find(atomIndices.begin(), atomIndices.end(), a->getIdx());
-    auto bIt = std::find(atomIndices.begin(), atomIndices.end(), b->getIdx());
-    if (aIt != atomIndices.end() && bIt != atomIndices.end()) {
-      if (connectOBMol(a, b, dist_sq, covFactor)) {
-        // we need to add the bond to newMol, so we need to get the index of
-        // the atoms in newMol that correspond to a and b
-        int indexA = std::distance(atomIndices.begin(), aIt);
-        int indexB = std::distance(atomIndices.begin(), bIt);
-        newMol.addBond(indexA, indexB, RDKit::Bond::BondType::SINGLE);
-      }
-    }
-  }
-}
-
-void perceiveBonds(RDKit::RWMol &mol, const NSResults &results,
-                   const float covFactor) {
-
-  for (auto i = 0; i < results.getNeighbors().size(); i++) {
-    auto res = results.getNeighbors()[i];
-    auto dist_sq = results.distances[i];
-
-    auto *a = mol.getAtomWithIdx(res.first);
-    auto *b = mol.getAtomWithIdx(res.second);
-
-    // FIXME: Needs to be tested if it is necessary, if so when
-    // if (shouldSkip(*a) || shouldSkip(*b)) {
-    //   continue;
-    // }
-
-    if (connectOBMol(a, b, dist_sq, covFactor)) {
-      // FIX: Neighbor indices are guaranteed to be unique, so we don't need
-      // this.
-      if (mol.getBondBetweenAtoms(a->getIdx(), b->getIdx()) != nullptr) {
-        return;
-      }
-      mol.addBond(a->getIdx(), b->getIdx(), RDKit::Bond::BondType::SINGLE);
-    }
-  }
-}
-
-// TODO: Implement a struct to hold both RWMol and the non-protein indices
-// the non-protein indices should perhaps be put into a set first, to avoid
+// TODO: Implement a struct to hold both RWMol and the non-protein indices the
+// non-protein indices should perhaps be put into a set first, to avoid
 // duplicates.
 // Function to collect unique indices during iteration over neighboring pairs
 RDKit::RWMol lahutaBondAssignment(RDKit::RWMol &mol, const NSResults &results,
