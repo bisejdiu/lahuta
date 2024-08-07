9a118eab3a89795a9ba10fc6ef537d27a1086875
Besian I. Sejdiu <besiansejdiu@gmail.com>
2024-08-07 01:24:22 -0500

    improved the starting point for the python bindings and got correct stubs to get generated for the bindings

--- python/lahuta.cpp
+++ python/lahuta.cpp
@@ -1,60 +1,45 @@
 #include "lahuta.hpp"
 #include "nsgrid.hpp"
+#include <GraphMol/RDKitBase.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
+// using namespace Lahuta;
 
-// // Code to write Python bindings
-// std::string file_name = "1a1e.cif.gz";
-// Structure st = read_structure_gz(file_name);
-// Lahuta::GemmiSource source = Lahuta::GemmiSource();
-// source.process(st);
-// Lahuta::Luni luni(source);
-
-//
-// py::class_<Lahuta::ISource>(m, "ISource")
-//    .def("getMolecule", &Lahuta::ISource::getMolecule)
-//    .def("getConformer", &Lahuta::ISource::getConformer)
-//    .def("process", &Lahuta::ISource::process);
-
-void test_lahuta(py::module &LT) {
-  // ISource is a virtual class, so we can't instantiate it
-  py::class_<Lahuta::GemmiSource> GemmiSource(LT, "GemmiSource");
-  py::class_<Lahuta::Luni> Luni(LT, "Luni");
-
-  // py::class_<Lahuta::GemmiSource, Lahuta::ISource>(LT, "GemmiSource")
-  //     .def(py::init<>())
-  //     .def("process", &Lahuta::GemmiSource::process);
+void test_lahuta(py::module &_lahuta) {
+  py::class_<Lahuta::GemmiSource> GemmiSource(_lahuta, "GemmiSource");
+  py::class_<Lahuta::Luni> Luni(_lahuta, "Luni");
 
   GemmiSource
-      // .def(py::init<>(), py::arg("source") = Lahuta::GemmiSource())
       .def(py::init<>())
-      .def("process", &Lahuta::GemmiSource::process);
-      // .def("getMolecule", &Lahuta::GemmiSource::getMolecule)
-      // .def_readonly("getMolecule", &Lahuta::GemmiSource::getMolecule)
-      // .def("getConformer", &Lahuta::GemmiSource::getConformer);
+      .def("process", &Lahuta::GemmiSource::process)
+      .def("get_conformer", &Lahuta::GemmiSource::get_conformer);
+  // .def("get_molecule",
+  //      (RDKit::RWMol & (GemmiSource::*)()) & GemmiSource::get_molecule)
+  // .def("get_molecule", (const RDKit::RWMol &(GemmiSource::*)() const) &
+  //                          GemmiSource::get_molecule);
 
-  Luni
-      // .def(py::init<Lahuta::GemmiSource>(), py::return_value_policy::copy)
-      .def(py::init<std::string>())
-      .def("getNeighborResults", &Lahuta::Luni::get_neighbors)
-      .def("findNeighbors", &Lahuta::Luni::find_neighbors)
-      .def("getCutoff", &Lahuta::Luni::get_cutoff);
-      // .def("getMolecule", &Lahuta::Luni::getMolecule);
+  Luni.def(py::init<std::string>())
+      .def("get_neighbors", &Lahuta::Luni::get_neighbors)
+      .def("find_neighbors", &Lahuta::Luni::find_neighbors)
+      .def("get_cutoff", &Lahuta::Luni::get_cutoff);
 
-  py::class_<NSResults>(LT, "NSResults")
+  py::class_<NSResults>(_lahuta, "NSResults")
       .def(py::init<>())
-      .def("addNeighbors", &NSResults::add_neighbors)
-      .def("reserveSpace", &NSResults::reserve_space)
-      .def("getNeighbors", &NSResults::get_neighbors)
-      .def("getNeighborPairsSize", &NSResults::size)
-      .def("filterByDistance", &NSResults::filter);
+      .def("add_neighbors", &NSResults::add_neighbors)
+      .def("reserve_space", &NSResults::reserve_space)
+      .def("get_neighbors", &NSResults::get_neighbors)
+      .def("size", &NSResults::size)
+      .def("filter", &NSResults::filter);
 }
 
 
 PYBIND11_MODULE(_lahuta, m) {
   m.doc() = "Lahuta: A Python binding for the Lahuta library";
+
+  py::class_<RDKit::RWMol> LahutaRWMol(m, "RWMol");
+  py::class_<RDKit::Conformer> LahutaConformer(m, "Conformer");
+
   test_lahuta(m);
 }
-
