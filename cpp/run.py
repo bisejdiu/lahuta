049dc1111b45fad184a29d1fefb76da32e7c6bc6
bisejdiu <besian.sejdiu@stjude.org>
2025-08-12 21:02:34 -0500

    Updated Project Directory & File Organization
    This commit changes the directory structure. The old system resulted in
    duplicated file trees and split definition from source. Now everything
    is co-localized. This reorg, thankfully, did not need any changes to
    include statements.
    
    I also deleted a few files that I'd forgotten to delete when merging
    old PRs.

--- cpp/run.py
+++
@@ -1,9 +0,0 @@
-import sys
-from _lahuta import Luni
-
-luni = Luni(sys.argv[1])
-ns = luni.get_neighbors()
-print(ns.size())
-print(len(ns.get_neighbors()))
-print(ns.get_neighbors()[:10])
-print(luni.get_cutoff())
