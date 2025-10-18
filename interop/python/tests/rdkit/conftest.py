94f66c3489bc827635bd78561fc92e63fc57c890
bisejdiu <besian.sejdiu@stjude.org>
2025-10-18 05:15:19 -0500

    Disallow skipping RDKit tests if bindings are not available

--- interop/python/tests/rdkit/conftest.py
+++ interop/python/tests/rdkit/conftest.py
@@ -5,7 +5,4 @@ import pytest
 
 @pytest.fixture(scope="module")
 def rdkit():
-    try:
-        return importlib.import_module("lahuta.rdkit")
-    except ImportError:
-        pytest.skip("C++ bindings not available! rdkit tests skipped")
+    return importlib.import_module("lahuta.rdkit")
