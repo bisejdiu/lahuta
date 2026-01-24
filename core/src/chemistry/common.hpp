30451452b8c19b0ac483576c001390e5d0a92c90
bisejdiu <besian.sejdiu@stjude.org>
2026-01-23 23:40:14 -0600

    removed old factorizer

--- core/src/chemistry/common.hpp
+++ core/src/chemistry/common.hpp
@@ -7,8 +7,6 @@
 #include <rdkit/GraphMol/MonomerInfo.h>
 #include <rdkit/GraphMol/RWMol.h>
 
-#include "logging/logging.hpp"
-
 namespace lahuta {
 
 struct AtomInfo {
@@ -47,61 +45,6 @@ inline bool has_any_aromatic_atom(const RDKit::RWMol &mol, const RDKit::INT_VECT
   return std::any_of(ring.begin(), ring.end(), [&mol](int idx) {
     return mol.getAtomWithIdx(idx)->getIsAromatic();
   });
-}
-
-inline void log_atom_info(const RDKit::Atom *atom) {
-  auto *info = static_cast<const RDKit::AtomPDBResidueInfo *>(atom->getMonomerInfo());
-  if (info) {
-    Logger::get_logger()->info(
-        "Atom: {} {} {} {} {}",
-        atom->getIdx(),
-        info->getName(),
-        info->getResidueName(),
-        info->getResidueNumber(),
-        info->getChainId());
-  } else {
-    Logger::get_logger()->trace("Atom: {} {}", atom->getIdx(), atom->getSymbol());
-  }
-}
-
-inline void log_bond_info(const RDKit::Bond *bond) {
-  auto *begin = bond->getBeginAtom();
-  auto *end = bond->getEndAtom();
-  auto *info_begin = static_cast<const RDKit::AtomPDBResidueInfo *>(begin->getMonomerInfo());
-  auto *info_end = static_cast<const RDKit::AtomPDBResidueInfo *>(end->getMonomerInfo());
-  if (info_begin && info_end) {
-    Logger::get_logger()->info(
-        "Bond: {}-{}-{}-{}-{} {}-{}-{}-{}-{}",
-        begin->getIdx(),
-        info_begin->getName(),
-        info_begin->getResidueName(),
-        info_begin->getResidueNumber(),
-        info_begin->getChainId(),
-        end->getIdx(),
-        info_end->getName(),
-        info_end->getResidueName(),
-        info_end->getResidueNumber(),
-        info_end->getChainId());
-  }
-}
-
-inline void log_bond_info(const RDKit::Atom *a1, const RDKit::Atom *a2) {
-  auto *info_a1 = static_cast<const RDKit::AtomPDBResidueInfo *>(a1->getMonomerInfo());
-  auto *info_a2 = static_cast<const RDKit::AtomPDBResidueInfo *>(a2->getMonomerInfo());
-  if (info_a1 && info_a2) {
-    Logger::get_logger()->info(
-        "Bond: {}-{}-{}-{}-{} {}-{}-{}-{}-{}",
-        a1->getIdx(),
-        info_a1->getName(),
-        info_a1->getResidueName(),
-        info_a1->getResidueNumber(),
-        info_a1->getChainId(),
-        a2->getIdx(),
-        info_a2->getName(),
-        info_a2->getResidueName(),
-        info_a2->getResidueNumber(),
-        info_a2->getChainId());
-  }
 }
 
 } // namespace common
---
+++ core/src/utils/debug_helpers.hpp
@@ -0,0 +1,69 @@
+#ifndef LAHUTA_DEBUG_HELPERS_HPP
+#define LAHUTA_DEBUG_HELPERS_HPP
+
+#include <rdkit/GraphMol/Bond.h>
+#include <rdkit/GraphMol/MonomerInfo.h>
+#include <rdkit/GraphMol/RWMol.h>
+
+#include "logging/logging.hpp"
+
+namespace lahuta {
+
+inline void log_atom_info(const RDKit::Atom *atom) {
+  auto *info = static_cast<const RDKit::AtomPDBResidueInfo *>(atom->getMonomerInfo());
+  if (info) {
+    Logger::get_logger()->info(
+        "Atom: {} {} {} {} {}",
+        atom->getIdx(),
+        info->getName(),
+        info->getResidueName(),
+        info->getResidueNumber(),
+        info->getChainId());
+  } else {
+    Logger::get_logger()->trace("Atom: {} {}", atom->getIdx(), atom->getSymbol());
+  }
+}
+
+inline void log_bond_info(const RDKit::Bond *bond) {
+  auto *begin = bond->getBeginAtom();
+  auto *end = bond->getEndAtom();
+  auto *info_begin = static_cast<const RDKit::AtomPDBResidueInfo *>(begin->getMonomerInfo());
+  auto *info_end = static_cast<const RDKit::AtomPDBResidueInfo *>(end->getMonomerInfo());
+  if (info_begin && info_end) {
+    Logger::get_logger()->info(
+        "Bond: {}-{}-{}-{}-{} {}-{}-{}-{}-{}",
+        begin->getIdx(),
+        info_begin->getName(),
+        info_begin->getResidueName(),
+        info_begin->getResidueNumber(),
+        info_begin->getChainId(),
+        end->getIdx(),
+        info_end->getName(),
+        info_end->getResidueName(),
+        info_end->getResidueNumber(),
+        info_end->getChainId());
+  }
+}
+
+inline void log_bond_info(const RDKit::Atom *a1, const RDKit::Atom *a2) {
+  auto *info_a1 = static_cast<const RDKit::AtomPDBResidueInfo *>(a1->getMonomerInfo());
+  auto *info_a2 = static_cast<const RDKit::AtomPDBResidueInfo *>(a2->getMonomerInfo());
+  if (info_a1 && info_a2) {
+    Logger::get_logger()->info(
+        "Bond: {}-{}-{}-{}-{} {}-{}-{}-{}-{}",
+        a1->getIdx(),
+        info_a1->getName(),
+        info_a1->getResidueName(),
+        info_a1->getResidueNumber(),
+        info_a1->getChainId(),
+        a2->getIdx(),
+        info_a2->getName(),
+        info_a2->getResidueName(),
+        info_a2->getResidueNumber(),
+        info_a2->getChainId());
+  }
+}
+
+} // namespace lahuta
+
+#endif // LAHUTA_DEBUG_HELPERS_HPP
